d5e504934543d000a7b70ad8508287d1f6a8bedb
INTI0384 <INTI0384@DESKTOP-9NBIJ4O.home>
2017-08-31 09:57:09 +0200

    pojo user et annotation + dao

---
+++ src/main/java/fr/adaming/springbootangular/model/User.java
@@ -0,0 +1,63 @@
+package fr.adaming.springbootangular.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+	private Long idUser;
+	private String login;
+	private String password;
+
+	public User() {
+		super();
+	}
+
+	public User(Long idUser, String login, String password) {
+		super();
+		this.idUser = idUser;
+		this.login = login;
+		this.password = password;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_User")
+	public Long getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(Long idUser) {
+		this.idUser = idUser;
+	}
+
+	@Column(name = "login")
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	@Column(name = "password")
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [idUser=" + idUser + ", login=" + login + ", password=" + password + "]";
+	}
+
+}
