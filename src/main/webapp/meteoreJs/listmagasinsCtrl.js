02bc17236c54c7c025d7633c7f542c1fc43970ac
INTI-0377 <INTI-0377@DESKTOP-5M76TR3.home>
2017-09-01 12:41:49 +0200

    magasins

--- src/main/webapp/meteoreJs/listmagasinsCtrl.js
+++
@@ -1,9 +0,0 @@
-
-var app=angular.module('metoreApp');
-
- var ctrl=app.controller('listmagasinsCtrl',  function($scope,$window) {
-   $scope.loginFct=function() {
-	   $window.location.href = '/listmagasins.html';
-   }
-});
- 
