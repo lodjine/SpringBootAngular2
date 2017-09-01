c49f43c2d79a9294fbf72d8f2fb9cd22521c7e70
Franck <INTI0379@DESKTOP-LIA5UUP.home>
2017-09-01 14:43:46 +0200

    listProduits modif

--- src/main/webapp/meteoreJs/listProduitsCtrl.js
+++ src/main/webapp/meteoreJs/listProduitsCtrl.js
@@ -1,11 +1,13 @@
 
+'use strict';
+
 angular.module('meteoreApp').controller('listProduitsCtrl', listProduitsCtrl);
 
 listProduitsCtrl.$inject = ['$scope', 'Produit'];
 
 	function listProduitsCtrl($scope, Produit){
-		
+		$scope.listProduits=Produit.querry();
 	}
   
-});
+};
  
--- src/main/webapp/meteoreJs/listProduitsService.js
+++ src/main/webapp/meteoreJs/listProduitsService.js
@@ -1,9 +1,11 @@
+'use strict';
+
 angular.module('meteoreApp').factory('Produit', Produit);
 
 Produit.$inject = [ '$resource' ];
 
 function listProduits($resource) {
-	var resourceUrl = '/produits';
+	var resourceUrl = '/produits/:id';
 	return $resource(resourceUrl, {}, {});
 
 }
