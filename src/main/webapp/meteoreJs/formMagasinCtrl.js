76f7fae4938cb4c2e50d3d213543ef27db2ee252
Robin <INTI0356@DESKTOP-RD4ESAG.home>
2017-09-01 11:55:54 +0200

    MAJ form magasin

--- src/main/webapp/meteoreJs/formMagasinCtrl.js
+++ src/main/webapp/meteoreJs/formMagasinCtrl.js
@@ -3,20 +3,19 @@
  */
 var app = angular.module('meteoreApp');
 
-var ctrl = app.controller('formMagasinCtrl', function($scope,$window){
-	
-	$scope.validationForm()=function(){
-		if ($scope.nom.length > 3 && $scope.adresse.length > 10 ){
+var ctrl = app.controller('formMagasinCtrl', function($scope, $window){
+	$scope.message="";
+	$scope.validationForm = function(){
+		if ($scope.nom == "3" && $scope.adresse == "3" ){
 			$window.location.href = '/accueil.html';
 		}	
-			$scope.message = "Erreur, adresse et/ou nom incorrect !";
+			$scope.message = "Erreur : adresse et/ou nom incorrect !";
 		else {
 			$window.location.href = '/accueil.html';
 		}
-
 	}
 	
-	$scope.annuler()=function(){
-		$window.location.href='/accueil.html';
+	$scope.annuler = function(){
+		$window.location.href = '/accueil.html';
 	}
 });
