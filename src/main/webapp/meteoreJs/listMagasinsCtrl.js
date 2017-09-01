a1ddafb89a3cf456ee8e00a2478b09a950a91c86
INTI0356 <INTI0356@DESKTOP-RD4ESAG.home>
2017-09-01 14:27:32 +0200

    maj

--- src/main/webapp/meteoreJs/listMagasinsCtrl.js
+++
@@ -1,9 +0,0 @@
-
-var app=angular.module('metoreApp');
-
- var ctrl=app.controller('listMagasinsCtrl',  function($scope,$window) {
-   $scope.listMagasinsFct=function() {
-	   $window.location.href = '/listMagasins.html';
-   }
-});
- 
