86809a99e6d54470bb3af39c02a38b85aa4b5df2
INTI0356 <INTI0356@DESKTOP-RD4ESAG.home>
2017-09-04 10:15:24 +0200

    maj formulaire magasin

--- src/main/webapp/meteoreJs/formMagasinCtrl.js
+++ src/main/webapp/meteoreJs/formMagasinCtrl.js
@@ -4,13 +4,11 @@ formMagasinCtrl.$inject = [ '$scope', 'MagasinFormService' ];
 
 function formMagasinCtrl($scope, MagasinFormService, $window) {
 
-	var fr = $scope.magasin.nom;
-
 	$scope.validationFct = function(magasin) {
-		$scope.message = "$scope.magasin.nom";
-		MagasinFormService.save(magasin);
 
-		$window.location.href = "/accueil.html";
+		MagasinFormService.save(magasin);
+		$window.location.href = '/accueil.html';
+
 	}
 
 };
