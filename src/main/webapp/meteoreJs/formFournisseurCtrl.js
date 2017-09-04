d62036ce957ba3cc9d891aee4b75032f436f3a17
INTI0384 <INTI0384@DESKTOP-9NBIJ4O.home>
2017-09-04 17:05:07 +0200

    form fournisseur

---
+++ src/main/webapp/meteoreJs/formFournisseurCtrl.js
@@ -0,0 +1,15 @@
+angular.module('meteoreApp').controller('formFournisseurCtrl', formFournisseurCtrl);
+
+formMagasinCtrl.$inject = [ '$scope', 'formFournisseurCtrl', '$window' ];
+
+function formMagasinCtrl($scope, formFournisseurCtrl, $window) {
+
+	$scope.validationFct = function(fournisseur) {
+		formFournisseurCtrl.save(fournisseur);
+		$window.location.href = '/accueil.html#/listFournisseur';
+	}
+
+	$scope.annulerFct = function() {
+		$window.location.href = '/accueil.html#/listFournisseur';
+	}
+};
