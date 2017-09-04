c1a7ccba9b1cd7d932050057efe2f4af38ad7354
Robin <INTI0356@DESKTOP-RD4ESAG.home>
2017-09-04 11:55:41 +0200

    Formulaire opérationnel

--- src/main/webapp/meteoreJs/formMagasinCtrl.js
+++ src/main/webapp/meteoreJs/formMagasinCtrl.js
@@ -6,10 +6,12 @@ function formMagasinCtrl($scope, MagasinFormService, $window) {
 	$scope.message = "bla";
 
 	$scope.validationFct = function(magasin) {
-
 		MagasinFormService.save(magasin);
 		$window.location.href = '/accueil.html';
+	}
 
+	$scope.annuler = function() {
+		$window.location.href = "/accueil.html";
 	}
 
 };
