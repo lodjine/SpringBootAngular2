f7d2febe7aa7766aa159c54b4a965adfe9968130
Robin <INTI0356@DESKTOP-RD4ESAG.home>
2017-09-04 10:35:22 +0200

    maj formulaire lundi

--- src/main/webapp/meteoreJs/formMagasinCtrl.js
+++ src/main/webapp/meteoreJs/formMagasinCtrl.js
@@ -3,11 +3,11 @@ angular.module('meteoreApp').controller('formMagasinCtrl', formMagasinCtrl);
 formMagasinCtrl.$inject = [ '$scope', 'MagasinFormService' ];
 
 function formMagasinCtrl($scope, MagasinFormService, $window) {
-
+	$scope.message = "bla";
+	
 	$scope.validationFct = function(magasin) {
 
 		MagasinFormService.save(magasin);
-		$window.location.href = '/accueil.html';
 
 	}
 
