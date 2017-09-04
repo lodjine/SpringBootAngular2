092a73e8de1b15ee8fde630e31f75b8fd551ab71
Franck <INTI0379@DESKTOP-LIA5UUP.home>
2017-09-04 12:40:53 +0200

    listProduitsCtrl

--- src/main/webapp/meteoreJs/listProduitsCtrl.js
+++ src/main/webapp/meteoreJs/listProduitsCtrl.js
@@ -3,7 +3,7 @@ angular.module('meteoreApp').controller('listProduitsCtrl', listProduitsCtrl);
 
 listProduitsCtrl.$inject = ['$scope','ProduitsService'];
 
-	function listProduitsCtrl($scope,ProduitsService ){
+	function listProduitsCtrl($scope,ProduitsService){
 	
 		$scope.produits=ProduitsService.query();
 		
