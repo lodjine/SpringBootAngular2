7898ee8a19f9c6bcdd7b8220f7bccbd8dc618071
INTI0356 <INTI0356@DESKTOP-RD4ESAG.home>
2017-09-04 10:51:12 +0200

    formulaire magasin (foncionnel)

--- src/main/webapp/meteoreJs/formMagasinCtrl.js
+++ src/main/webapp/meteoreJs/formMagasinCtrl.js
@@ -1,13 +1,14 @@
 angular.module('meteoreApp').controller('formMagasinCtrl', formMagasinCtrl);
 
-formMagasinCtrl.$inject = [ '$scope', 'MagasinFormService' ];
+formMagasinCtrl.$inject = [ '$scope', 'MagasinFormService', '$window' ];
 
 function formMagasinCtrl($scope, MagasinFormService, $window) {
 	$scope.message = "bla";
-	
+
 	$scope.validationFct = function(magasin) {
 
 		MagasinFormService.save(magasin);
+		$window.location.href = '/accueil.html';
 
 	}
 
